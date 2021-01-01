85c38dc845f931f85f068587025fa6bc764531a6
BoBoBalloon <61363161+BoBoBalloon@users.noreply.github.com>
2020-12-31 19:25:42 -0500

    git init

---
+++ src/main/java/me/boboballoon/stunningskins/StunningSkins.java
@@ -0,0 +1,17 @@
+package me.boboballoon.stunningskins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StunningSkins extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
